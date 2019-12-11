fec3629f573fdd2e58c8202bc7d01918c187506e
wintus <yuya0726+GitHub@gmail.com>
2019-12-11 15:13:49 +0900

    Replace camera by reader

--- BarcodeScannerTrial/App.tsx
+++ BarcodeScannerTrial/App.tsx
@@ -8,69 +8,33 @@
  * @format
  */
 
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {RNCamera, RNCameraProps} from 'react-native-camera';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
 const SampleCamera: React.FC = () => {
+  const [barcode, setBarcode] = useState('');
+
   return (
     <View style={styles.container}>
       <RNCamera
         style={styles.preview}
         type={RNCamera.Constants.Type.back}
         flashMode={RNCamera.Constants.FlashMode.on}
+        onBarCodeRead={({data, type, rawData}) => {
+          console.log({barcode: {data, type, rawData}});
+          setBarcode(data);
+        }}
         androidCameraPermissionOptions={{
           title: 'Permission to use camera',
           message: 'We need your permission to use your camera',
           buttonPositive: 'Ok',
           buttonNegative: 'Cancel',
         }}
-        androidRecordAudioPermissionOptions={{
-          title: 'Permission to use audio recording',
-          message: 'We need your permission to use your audio',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}>
-        {child}
-      </RNCamera>
+      />
+      <Text style={styles.code}>{barcode}</Text>
     </View>
   );
-};
-
-type CameraChild = Extract<NonNullable<RNCameraProps['children']>, Function>;
-
-const child: CameraChild = ({camera, status}) => {
-  if (status !== 'READY') {
-    return <PendingView />;
-  }
-
-  return (
-    <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
-      <TouchableOpacity
-        onPress={() => takePicture(camera)}
-        style={styles.capture}>
-        <Text style={{fontSize: 14}}> SNAP </Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-const PendingView = () => (
-  <View
-    style={{
-      flex: 1,
-      backgroundColor: 'lightgreen',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-    <Text>Waiting</Text>
-  </View>
-);
-
-const takePicture = async (camera: RNCamera) => {
-  const options = {quality: 0.5, base64: true};
-  const data = await camera.takePictureAsync(options);
-  console.log(data.uri);
 };
 
 const styles = StyleSheet.create({
@@ -92,6 +56,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     alignSelf: 'center',
     margin: 20,
+  },
+  code: {
+    margin: 10,
+    color: 'red',
   },
 });
 
